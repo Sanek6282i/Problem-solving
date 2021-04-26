6a3e20f280187f15c973ed45ceb3251076cf89e2
AleksandrVlasov <c6282i@gmail.com>
2021-04-26 21:28:32 +0300

    edit gitignore

--- other_problem/spiral.py
+++ other_problem/spiral.py
@@ -6,42 +6,32 @@ starting from the upper left corner and twisted clockwise
 n = int(input())
 matrix = [[0] * n for _ in range(n)]  # zero matrix n x n
 counter = 1
-start_i = 0
-start_j = 0
-end_i = n - 1
-end_j = n - 1
+si = 0
+sj = 0
+ei = n - 1
+ej = n - 1
 flag = True
-for i in range(n):  # first line
-    matrix[0][i] = counter
-    counter += 1
 while flag:
-    start_i += 1
-    for i in range(start_i, end_i + 1):  # j = end_j
-        if counter > n ** 2:
-            flag = False
-            break
-        matrix[i][end_j ] = counter
+    for i in range(n):  # first line
+        matrix[0][i] = counter
         counter += 1
-    end_j -= 1
-    for j in range(end_j, start_j, -1):  # i = end_i
+    for i in range(si, ei + 1):  # j = end_j
         if counter > n ** 2:
             flag = False
             break
-        matrix[end_i][j] = counter
+        matrix[i][ej] = counter
         counter += 1
-    start_i += 1
-    for i in range(end_i, start_j, -1):  # j = start_j
+    for j in range(ej, sj, -1):  # i = end_i
         if counter > n ** 2:
             flag = False
             break
-        matrix[i][start_j] = counter
+        matrix[ei][j] = counter
         counter += 1
-    start_j += 1
-    for j in range(start_j - 1, end_j):  # i = start_i
+    for i in range(ei, sj, -1):  # j = start_j
         if counter > n ** 2:
             flag = False
             break
-        matrix[start_i][j] = counter
+        matrix[i][sj] = counter
         counter += 1
 
 print(*matrix, sep='\n')
